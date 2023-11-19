524f6b0d0fb444d7fbf6f432949ff67694e4da36
ZLozZ <ironman242k2@gmail.com>
2023-11-19 22:38:45 +0700

    add algorithm mutual recursion

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include "stdio.h"
+#include "stdbool.h"
 
-int main(){
+bool isEven(int n);
+
+bool isOdd(int n);
+
+bool isEven(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return isOdd(n - 1);
+}
+
+bool isOdd(int n) {
+    return !isEven(n);
+}
+
+int main() {
+    int n1 = 9;
+    bool kq = isEven(n1);
+    printf("%d", kq);
     return 0;
 }
