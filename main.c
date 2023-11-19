996f4652dae24a4e6b9f8e5522d85d58366b5477
ZLozZ <ironman242k2@gmail.com>
2023-11-19 22:19:44 +0700

    add algorithm nested recursion

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "stdio.h"
 
+int ackerman(int m, int n){
+    if(m==0) return n+1;
+    else if(n==0)
+        return ackerman(m-1,1);
+    else
+        return ackerman(m-1, ackerman(m,n-1));
+}
+
 int main(){
+    int result = ackerman(1,1);
+    printf("Result: %d", result);
     return 0;
 }
