af3e8b36033358865ce652c18fb2eb09b6018519
ZLozZ <ironman242k2@gmail.com>
2023-11-19 16:48:28 +0700

    add algorithm binary recursion

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "stdio.h"
 
+int feb(int n){
+    if(n<=2) return 1;
+    return feb(n-1)+feb(n-2);
+}
+
 int main(){
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    int result = feb(n);
+    printf("Result: %d", result);
     return 0;
 }
