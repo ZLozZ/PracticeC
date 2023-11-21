77f232efd476babd6403d569550adb699e5a8427
ZLozZ <ironman242k2@gmail.com>
2023-11-21 21:22:30 +0700

    add algorithm insertion sort

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include "stdio.h"
 
-int main(){
+void insertionSort(int *arr, int n) {
+    int pos = 0;
+    int x;
+    for (int i = 0; i < n; i++) {
+        x = arr[i];
+        for (pos = i; (pos > 0) && (arr[pos - 1] > x); pos--) {
+            arr[pos] = arr[pos - 1];
+        }
+        arr[pos] = x;
+    }
+}
+
+void xuatMang(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
+    }
+}
+
+int main() {
+    int arr[] = {10, 8, 20, 81, 25};
+    insertionSort(arr, 5);
+    xuatMang(arr, 5);
     return 0;
 }
