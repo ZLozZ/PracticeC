9ad979170a761497ddf5725f32a7ef0e55533b08
ZLozZ <ironman242k2@gmail.com>
2023-11-21 21:51:42 +0700

    add algorithm interchange sort

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include "stdio.h"
 
+void interChangeSort(int *arr, int n){
+    int i, j;
+    for(i = 0; i<n-1;i++){
+        for(j = i+1; j < n; j++){
+            if(arr[j] < arr[i]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void xuatMang(int *arr, int n){
+    for(int i = 0; i<n;i++){
+        printf("%d\t", arr[i]);
+    }
+}
+
 int main(){
+    int arr[] = {100, 3, 60, 35,2};
+    interChangeSort(arr,5);
+    xuatMang(arr,5);
     return 0;
 }
