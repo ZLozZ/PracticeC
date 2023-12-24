685f4b9c2c29884407b09030bc87e58b7e1c319f
ZLozZ <ironman242k2@gmail.com>
2023-12-24 14:45:37 +0700

    add file code linked list

--- main.c
+++ main.c
@@ -1,5 +1,52 @@
+#include <malloc.h>
 #include "stdio.h"
 
-int main(){
+typedef struct Node {
+    int value;
+    struct Node *pNext;
+} Node_t;
+
+typedef struct SingleList {
+    Node_t *pHead;
+} SingleList_t;
+
+void intialize(SingleList_t *list) {
+    list->pHead = NULL;
+}
+
+Node_t *CreateNode(int d) {
+    Node_t *pNode = (Node_t *) malloc(sizeof(Node_t));
+    if (pNode != NULL) {
+        pNode->value = d;
+        pNode->pNext = NULL;
+    } else {
+        printf("Error allocated memory");
+    }
+    return pNode;
+}
+
+void printList(SingleList_t list) {
+    Node_t *pTmp = list.pHead;
+    if (pTmp == NULL) {
+        printf("The list is emptry");
+        return;
+    }
+    while (pTmp != NULL) {
+        printf("%d", pTmp->value);
+        pTmp = pTmp->pNext;
+    }
+}
+
+int sizeOfList(SingleList_t list){
+    Node_t *pTmp = list.pHead;
+    int nSize = 0;
+    while(pTmp!=NULL){
+        pTmp = pTmp->pNext;
+        nSize++;
+    }
+    return nSize;
+}
+
+int main() {
     return 0;
 }
